4e2ce5cd44b7b79adcd9a76eaaaac20cc75ae0f3
adaussy <arthur.daussy@atos.net>
2013-10-29 17:57:50 +0100

    Bug 420319 - Update EASE maven configurations https://bugs.eclipse.org/bugs/show_bug.cgi?id=420319
    Same as before...

--- Releng/org.eclipse.ease.releng/after_build.sh
+++ Releng/org.eclipse.ease.releng/after_build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-targetUpdateSiteLocation=/home/data/httpd/download.eclipse.org/e4/ease/${JOB_NAME}/
+targetUpdateSiteLocation=/home/data/httpd/download.eclipse.org/e4/ease/update_site/${JOB_NAME}/
 sourceUpdateSiteLocation=${WORKSPACE}/Releng/org.eclipse.ease.releng.p2/target/repository
 #Clear all build
 echo "Clearing $targetUpdateSiteLocation"
