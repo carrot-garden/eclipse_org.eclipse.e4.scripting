4ac0aaa36eb83b35964e3bd8897b50fa36c98624
adaussy <arthur.daussy@atos.net>
2013-10-30 09:45:55 +0100

    Bug 420319 - Update EASE maven configurations https://bugs.eclipse.org/bugs/show_bug.cgi?id=420319
    Add upload of discovery

--- Releng/org.eclipse.ease.releng/after_build.sh
+++ Releng/org.eclipse.ease.releng/after_build.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-targetUpdateSiteLocation=/home/data/httpd/download.eclipse.org/e4/ease/update_site/${JOB_NAME}/
-sourceUpdateSiteLocation=${WORKSPACE}/Releng/org.eclipse.ease.releng.p2/target/repository
+targetUpdateSiteLocation=/home/data/httpd/download.eclipse.org/e4/updates/ease/${JOB_NAME}/
+sourceUpdateSiteLocation=${WORKSPACE}/Releng/org.eclipse.ease.releng.p2/target/repository/
 #Clear all build
 echo "Clearing $targetUpdateSiteLocation"
-rm -rf ${targetUpdateSiteLocation}/*
+rm -rf ${targetUpdateSiteLocation}*
 #Moving generated update site
 echo "Moving $sourceUpdateSiteLocation to $targetUpdateSiteLocation"
 mv ${sourceUpdateSiteLocation}* ${targetUpdateSiteLocation}
+
+#Publish dicovery
+sourceDiscoveryLocation=Plugins/core/org.eclipse.ease.discovery/DiscoveryDefinition.xmi
+mv ${sourceDiscoveryLocation}* ${targetUpdateSiteLocation}
