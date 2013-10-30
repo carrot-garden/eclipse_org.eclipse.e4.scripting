1f0bdab9297b48e3ece8875adf2f71da94f50bb3
adaussy <arthur.daussy@atos.net>
2013-10-30 10:35:07 +0100

    Bug 420319 - Update EASE maven configurations https://bugs.eclipse.org/bugs/show_bug.cgi?id=420319
    Correct error on script (again...)

--- Releng/org.eclipse.ease.releng/after_build.sh
+++ Releng/org.eclipse.ease.releng/after_build.sh
@@ -1,5 +1,15 @@
-﻿#!/bin/bash
-
+﻿#--------------------------------------------------------------------------------
+# Copyright (c) 2013 Atos
+#
+#    
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+#
+# Contributors:
+#    Arthur Daussy
+#--------------------------------------------------------------------------------
 targetUpdateSiteLocation=/home/data/httpd/download.eclipse.org/e4/updates/ease/${JOB_NAME}/
 sourceUpdateSiteLocation=${WORKSPACE}/Releng/org.eclipse.ease.releng.p2/target/repository/
 #Clear all build
