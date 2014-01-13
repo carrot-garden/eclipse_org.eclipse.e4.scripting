e5f461896de54e9d9d88788b3c11d64cd1ea9d1e
Christian Pontesegger <christian.pontesegger@web.de>
2014-01-13 11:14:42 +0100

    * added convertToScriptProject for JS

--- Plugins/engines/org.eclipse.ease.lang.javascript.examples/samples/scripts/ConvertToScriptProject.js
+++ Plugins/engines/org.eclipse.ease.lang.javascript.examples/samples/scripts/ConvertToScriptProject.js
@@ -4,11 +4,11 @@
  * Kudos: Arthur Daussy.
  * License: EPL 1.0
  * Description : {This script is used to convert the selected IProject to script project.}
- * EnableWhen::[With selection {
+ * VisibleWhen:[With selection {
  *    Iterable {
  *        InstanceOf "org.eclipse.core.resources.IProject"
  *    }
- *}]::
+ *}]
  */
 
 loadModule("WorkspaceResourceModule");
