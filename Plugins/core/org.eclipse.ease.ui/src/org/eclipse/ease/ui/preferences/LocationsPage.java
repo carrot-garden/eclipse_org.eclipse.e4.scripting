df5adfb261dcad735aa9997cebcab82c18c61c38
Christian Pontesegger <christian.pontesegger@web.de>
2014-04-06 14:32:05 +0200

    * use preferences to store locations
      do not store locations anymore in the EMF model. Instead use the model
      as simple cache.

--- Plugins/core/org.eclipse.ease.ui/src/org/eclipse/ease/ui/preferences/LocationsPage.java
+++ Plugins/core/org.eclipse.ease.ui/src/org/eclipse/ease/ui/preferences/LocationsPage.java
@@ -3,11 +3,14 @@ package org.eclipse.ease.ui.preferences;
 import java.io.File;
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.Set;
 
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.preferences.IEclipsePreferences;
+import org.eclipse.core.runtime.preferences.IPreferenceNodeVisitor;
 import org.eclipse.core.runtime.preferences.InstanceScope;
+import org.eclipse.ease.Logger;
 import org.eclipse.ease.ui.Activator;
 import org.eclipse.ease.ui.repository.IEntry;
 import org.eclipse.ease.ui.repository.ILocation;
@@ -16,40 +19,42 @@ import org.eclipse.ease.ui.scripts.repository.IRepositoryService;
 import org.eclipse.jface.dialogs.InputDialog;
 import org.eclipse.jface.layout.TableColumnLayout;
 import org.eclipse.jface.preference.PreferencePage;
+import org.eclipse.jface.resource.JFaceResources;
 import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.viewers.CellEditor;
+import org.eclipse.jface.viewers.CheckboxCellEditor;
 import org.eclipse.jface.viewers.ColumnLabelProvider;
 import org.eclipse.jface.viewers.ColumnWeightData;
+import org.eclipse.jface.viewers.EditingSupport;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.jface.viewers.TableViewerColumn;
+import org.eclipse.jface.viewers.TextCellEditor;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.jface.viewers.ViewerComparator;
-import org.eclipse.jface.viewers.ViewerFilter;
 import org.eclipse.jface.window.Window;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.DirectoryDialog;
-import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableColumn;
-import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.dialogs.ContainerSelectionDialog;
+import org.osgi.service.prefs.BackingStoreException;
 
 public class LocationsPage extends PreferencePage implements IWorkbenchPreferencePage {
-
-	private Text ftext;
 	private TableViewer tableViewer;
-	private Button btnUseCustomLocation;
+	private final Set<IEntry> fEntries = new HashSet<IEntry>();;
 
 	public LocationsPage() {
 	}
@@ -62,110 +67,107 @@ public class LocationsPage extends PreferencePage implements IWorkbenchPreferenc
 	@Override
 	protected Control createContents(final Composite parent) {
 		Composite container = new Composite(parent, SWT.NONE);
-		container.setLayout(new GridLayout(4, false));
-		{
-			Group grpScriptRecording = new Group(container, SWT.NONE);
-			grpScriptRecording.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 2, 1));
-			grpScriptRecording.setText("Script Recording");
-			GridLayout gl_grpScriptRecording = new GridLayout(4, false);
-			grpScriptRecording.setLayout(gl_grpScriptRecording);
-			new Label(grpScriptRecording, SWT.NONE);
-			{
-				btnUseCustomLocation = new Button(grpScriptRecording, SWT.CHECK);
-				btnUseCustomLocation.addSelectionListener(new SelectionAdapter() {
-					@Override
-					public void widgetSelected(final SelectionEvent e) {
-						// enable/disable components depending on checkbox
-						for (Control control : btnUseCustomLocation.getParent().getChildren()) {
-							if (!control.equals(btnUseCustomLocation))
-								control.setEnabled(btnUseCustomLocation.getSelection());
-						}
-					}
-				});
-				btnUseCustomLocation.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false, 3, 1));
-				btnUseCustomLocation.setText("Use custom storage location");
-			}
-			new Label(grpScriptRecording, SWT.NONE);
-			{
-				Label lblLocationUri = new Label(grpScriptRecording, SWT.NONE);
-				lblLocationUri.setEnabled(false);
-				lblLocationUri.setText("Location URI:");
-			}
-			{
-				ftext = new Text(grpScriptRecording, SWT.BORDER);
-				ftext.setEnabled(false);
-				ftext.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 2, 1));
-			}
-			new Label(grpScriptRecording, SWT.NONE);
-			new Label(grpScriptRecording, SWT.NONE);
-			{
-				Button btnWorkspace = new Button(grpScriptRecording, SWT.NONE);
-				btnWorkspace.addSelectionListener(new SelectionAdapter() {
-					@Override
-					public void widgetSelected(final SelectionEvent e) {
-						ContainerSelectionDialog dialog = new ContainerSelectionDialog(getShell(), ResourcesPlugin.getWorkspace().getRoot(), true,
-								"Select script storage folder");
-						if (dialog.open() == Window.OK) {
-							Object[] result = dialog.getResult();
-							if ((result.length > 0) && (result[0] instanceof IPath))
-								ftext.setText("workspace:/" + result[0].toString());
-						}
-					}
-				});
-				btnWorkspace.setEnabled(false);
-				btnWorkspace.setText("Workspace...");
-			}
-			{
-				Button btnFileSystem = new Button(grpScriptRecording, SWT.NONE);
-				btnFileSystem.addSelectionListener(new SelectionAdapter() {
-					@Override
-					public void widgetSelected(final SelectionEvent e) {
-						DirectoryDialog dialog = new DirectoryDialog(getShell());
-						String path = dialog.open();
-						if (path != null)
-							ftext.setText(new File(path).toURI().toString());
-					}
-				});
-				btnFileSystem.setEnabled(false);
-				btnFileSystem.setText("File System...");
-			}
-		}
-		new Label(container, SWT.NONE);
-		new Label(container, SWT.NONE);
+		container.setLayout(new GridLayout(2, false));
 		{
 			Label lblProvideLocationsTo = new Label(container, SWT.NONE);
-			GridData gd_lblProvideLocationsTo = new GridData(SWT.LEFT, SWT.CENTER, false, false, 4, 1);
-			gd_lblProvideLocationsTo.verticalIndent = 20;
-			lblProvideLocationsTo.setLayoutData(gd_lblProvideLocationsTo);
-			lblProvideLocationsTo.setText("Provide locations to look for scripts.");
+			lblProvideLocationsTo.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false, 2, 1));
+			lblProvideLocationsTo.setText("Provide locations to look for scripts. The default location will be used to store your recorded scripts.");
 		}
 		{
 			Composite composite = new Composite(container, SWT.NONE);
-			composite.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 5));
+			composite.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 6));
 			TableColumnLayout tcl_composite = new TableColumnLayout();
 			composite.setLayout(tcl_composite);
 			{
 				tableViewer = new TableViewer(composite, SWT.BORDER | SWT.FULL_SELECTION);
-				Table table = tableViewer.getTable();
+				final Table table = tableViewer.getTable();
 				table.setHeaderVisible(true);
 				table.setLinesVisible(true);
 				{
 					TableViewerColumn tableViewerColumn = new TableViewerColumn(tableViewer, SWT.NONE);
+					tableViewerColumn.setEditingSupport(new EditingSupport(tableViewer) {
+						@Override
+						protected boolean canEdit(final Object element) {
+							return true;
+						}
+
+						@Override
+						protected CellEditor getCellEditor(final Object element) {
+							return new TextCellEditor(table);
+						}
+
+						@Override
+						protected Object getValue(final Object element) {
+							if (element instanceof IEntry)
+								return ((IEntry) element).getLocation();
+
+							return "";
+						}
+
+						@Override
+						protected void setValue(final Object element, final Object value) {
+							if (element instanceof IEntry) {
+								((IEntry) element).setLocation(value.toString());
+								tableViewer.update(element, null);
+							}
+						}
+					});
 					TableColumn tblclmnLocation = tableViewerColumn.getColumn();
 					tcl_composite.setColumnData(tblclmnLocation, new ColumnWeightData(5, ColumnWeightData.MINIMUM_WIDTH, true));
 					tblclmnLocation.setText("Location");
 					tableViewerColumn.setLabelProvider(new ColumnLabelProvider() {
 						@Override
 						public String getText(final Object element) {
-							if (element instanceof IEntry)
-								return ((IEntry) element).getLocation();
+							if (element instanceof IEntry) {
+								if (((IEntry) element).isDefault())
+									return ((IEntry) element).getLocation() + " (default)";
+								else
+									return ((IEntry) element).getLocation();
+							}
 
 							return super.getText(element);
+						}
+
+						@Override
+						public Font getFont(final Object element) {
+							if (element instanceof IEntry) {
+								if (((IEntry) element).isDefault())
+									return JFaceResources.getFontRegistry().getBold(JFaceResources.DEFAULT_FONT);
+							}
+
+							return super.getFont(element);
 						}
 					});
 				}
 				{
 					TableViewerColumn tableViewerColumn = new TableViewerColumn(tableViewer, SWT.NONE);
+					tableViewerColumn.setEditingSupport(new EditingSupport(tableViewer) {
+						@Override
+						protected boolean canEdit(final Object element) {
+							return true;
+						}
+
+						@Override
+						protected CellEditor getCellEditor(final Object element) {
+							return new CheckboxCellEditor(table);
+						}
+
+						@Override
+						protected Object getValue(final Object element) {
+							if (element instanceof IEntry)
+								return ((IEntry) element).isRecursive();
+
+							return false;
+						}
+
+						@Override
+						protected void setValue(final Object element, final Object value) {
+							if (element instanceof IEntry) {
+								((IEntry) element).setRecursive((Boolean) value);
+								tableViewer.update(element, null);
+							}
+						}
+					});
 					TableColumn tblclmnRecursive = tableViewerColumn.getColumn();
 					tcl_composite.setColumnData(tblclmnRecursive, new ColumnWeightData(1, ColumnWeightData.MINIMUM_WIDTH, true));
 					tblclmnRecursive.setText("Recursive");
@@ -191,19 +193,7 @@ public class LocationsPage extends PreferencePage implements IWorkbenchPreferenc
 					}
 				});
 
-				tableViewer.setFilters(new ViewerFilter[] { new ViewerFilter() {
-
-					@Override
-					public boolean select(final Viewer viewer, final Object parentElement, final Object element) {
-						if (element instanceof IEntry)
-							return !((IEntry) element).isHidden();
-
-						return true;
-					}
-				} });
-
-				final IRepositoryService repositoryService = (IRepositoryService) PlatformUI.getWorkbench().getService(IRepositoryService.class);
-				tableViewer.setInput(new HashSet<IEntry>(repositoryService.getLocations()));
+				tableViewer.setInput(fEntries);
 			}
 		}
 		{
@@ -216,15 +206,13 @@ public class LocationsPage extends PreferencePage implements IWorkbenchPreferenc
 					if (dialog.open() == Window.OK) {
 						Object[] result = dialog.getResult();
 						if ((result.length > 0) && (result[0] instanceof IPath))
-							addEntry("workspace:/" + result[0].toString(), false);
+							addEntry("workspace:/" + result[0].toString());
 					}
 				}
 			});
 			btnAddWorkspace.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 1, 1));
 			btnAddWorkspace.setText("Add Workspace...");
 		}
-		new Label(container, SWT.NONE);
-		new Label(container, SWT.NONE);
 		{
 			Button btnAddFileSystem = new Button(container, SWT.NONE);
 			btnAddFileSystem.addSelectionListener(new SelectionAdapter() {
@@ -233,14 +221,12 @@ public class LocationsPage extends PreferencePage implements IWorkbenchPreferenc
 					DirectoryDialog dialog = new DirectoryDialog(getShell());
 					String path = dialog.open();
 					if (path != null)
-						addEntry(new File(path).toURI().toString(), false);
+						addEntry(new File(path).toURI().toString());
 				}
 			});
 			btnAddFileSystem.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 1, 1));
 			btnAddFileSystem.setText("Add File System...");
 		}
-		new Label(container, SWT.NONE);
-		new Label(container, SWT.NONE);
 		{
 			Button btnAddUri = new Button(container, SWT.NONE);
 			btnAddUri.addSelectionListener(new SelectionAdapter() {
@@ -248,27 +234,33 @@ public class LocationsPage extends PreferencePage implements IWorkbenchPreferenc
 				public void widgetSelected(final SelectionEvent e) {
 					InputDialog dialog = new InputDialog(getShell(), "Enter location URI", "Enter the URI of a location to add", "", new URIValidator());
 					if (dialog.open() == Window.OK)
-						addEntry(dialog.getValue(), false);
+						addEntry(dialog.getValue());
 				}
 			});
 			btnAddUri.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 1, 1));
 			btnAddUri.setText("Add URI...");
 		}
+		;
 		new Label(container, SWT.NONE);
-		new Label(container, SWT.NONE);
+		;
 		{
-			Button btnEdit = new Button(container, SWT.NONE);
-			btnEdit.addSelectionListener(new SelectionAdapter() {
+			Button btnSetAsDefault = new Button(container, SWT.NONE);
+			btnSetAsDefault.addSelectionListener(new SelectionAdapter() {
 				@Override
 				public void widgetSelected(final SelectionEvent e) {
-					// TODO implement edit functionality
+					IStructuredSelection selection = (IStructuredSelection) tableViewer.getSelection();
+					if (!selection.isEmpty()) {
+						Collection<IEntry> entries = (Collection<IEntry>) tableViewer.getInput();
+						for (IEntry entry : entries)
+							entry.setDefault(entry.equals(selection.getFirstElement()));
+					}
+
+					tableViewer.refresh();
 				}
 			});
-			btnEdit.setLayoutData(new GridData(SWT.FILL, SWT.BOTTOM, false, true, 1, 1));
-			btnEdit.setText("Edit");
+			btnSetAsDefault.setLayoutData(new GridData(SWT.FILL, SWT.BOTTOM, false, true, 1, 1));
+			btnSetAsDefault.setText("Default");
 		}
-		new Label(container, SWT.NONE);
-		new Label(container, SWT.NONE);
 		{
 			Button btnDelete = new Button(container, SWT.NONE);
 			btnDelete.addSelectionListener(new SelectionAdapter() {
@@ -276,10 +268,18 @@ public class LocationsPage extends PreferencePage implements IWorkbenchPreferenc
 				public void widgetSelected(final SelectionEvent e) {
 					IStructuredSelection selection = (IStructuredSelection) tableViewer.getSelection();
 					if (!selection.isEmpty()) {
-						Collection<IEntry> entries = (Collection<IEntry>) tableViewer.getInput();
 						for (Object location : selection.toList())
-							entries.remove(location);
+							fEntries.remove(location);
 
+						// verify that we have a default entry
+						boolean hasDefault = false;
+						for (IEntry entry : fEntries)
+							hasDefault |= entry.isDefault();
+
+						if ((!hasDefault) && (!fEntries.isEmpty()))
+							fEntries.iterator().next().setDefault(true);
+
+						// refresh UI
 						tableViewer.refresh();
 					}
 				}
@@ -287,42 +287,59 @@ public class LocationsPage extends PreferencePage implements IWorkbenchPreferenc
 			btnDelete.setLayoutData(new GridData(SWT.FILL, SWT.BOTTOM, false, false, 1, 1));
 			btnDelete.setText("Delete");
 		}
-		new Label(container, SWT.NONE);
-		new Label(container, SWT.NONE);
 
 		performDefaults();
 
 		return container;
 	}
 
-	private void addEntry(final String location, final boolean hidden) {
+	private void addEntry(final String location) {
+
 		IEntry entry = IRepositoryFactory.eINSTANCE.createEntry();
 		entry.setLocation(location);
 		entry.setRecursive(true);
-		entry.setHidden(hidden);
+		// first entry is also the default entry
+		entry.setDefault(fEntries.isEmpty());
 
-		Collection<IEntry> entries = (Collection<IEntry>) tableViewer.getInput();
-		entries.add(entry);
+		fEntries.add(entry);
 
 		tableViewer.refresh();
 	}
 
 	@Override
 	protected void performDefaults() {
-		IEclipsePreferences prefs = InstanceScope.INSTANCE.getNode(Activator.PLUGIN_ID);
-		String location = prefs.get(IPreferenceConstants.SCRIPT_STORAGE_LOCATION, IPreferenceConstants.DEFAULT_SCRIPT_STORAGE_LOCATION);
-		if (IPreferenceConstants.DEFAULT_SCRIPT_STORAGE_LOCATION.equals(location)) {
-			btnUseCustomLocation.setSelection(false);
 
-		} else {
-			btnUseCustomLocation.setSelection(true);
-			ftext.setText(location);
+		final IEclipsePreferences rootNode = InstanceScope.INSTANCE.getNode(Activator.PLUGIN_ID);
+
+		fEntries.clear();
+
+		try {
+			rootNode.accept(new IPreferenceNodeVisitor() {
+
+				@Override
+				public boolean visit(final IEclipsePreferences node) throws BackingStoreException {
+					if (rootNode.equals(node))
+						return true;
+
+					else {
+						IEntry entry = IRepositoryFactory.eINSTANCE.createEntry();
+						entry.setLocation(node.get("location", ""));
+						entry.setDefault(node.getBoolean("default", false));
+						entry.setRecursive(node.getBoolean("recursive", true));
+
+						fEntries.add(entry);
+
+						return false;
+					}
+				}
+			});
+		} catch (BackingStoreException e) {
+			// we were not able to load any locations, display empty view
 		}
 
-		for (Control control : btnUseCustomLocation.getParent().getChildren()) {
-			if (!control.equals(btnUseCustomLocation))
-				control.setEnabled(btnUseCustomLocation.getSelection());
-		}
+		// update UI
+		if (tableViewer != null)
+			tableViewer.refresh();
 
 		super.performDefaults();
 	}
@@ -330,33 +347,41 @@ public class LocationsPage extends PreferencePage implements IWorkbenchPreferenc
 	@Override
 	public boolean performOk() {
 
-		// store entries
+		// remove existing child nodes
+		final IEclipsePreferences rootNode = InstanceScope.INSTANCE.getNode(Activator.PLUGIN_ID);
+		try {
+			rootNode.accept(new IPreferenceNodeVisitor() {
+
+				@Override
+				public boolean visit(final IEclipsePreferences node) throws BackingStoreException {
+					if (rootNode.equals(node))
+						return true;
+
+					else {
+						node.removeNode();
+
+						return false;
+					}
+				}
+			});
+		} catch (BackingStoreException e) {
+			Logger.logError("Could not update script location preferences", e);
+			return false;
+		}
+
+		// add entries
+		for (IEntry entry : fEntries) {
+			String path = entry.getLocation().replace('/', '|');
+			IEclipsePreferences node = InstanceScope.INSTANCE.getNode(Activator.PLUGIN_ID + "/" + path);
+			node.put("location", entry.getLocation());
+			node.putBoolean("default", entry.isDefault());
+			node.putBoolean("recursive", entry.isRecursive());
+		}
+
+		// update repository
 		final IRepositoryService repositoryService = (IRepositoryService) PlatformUI.getWorkbench().getService(IRepositoryService.class);
-
-		Collection<IEntry> existing = repositoryService.getLocations();
-		for (IEntry entry : new HashSet<IEntry>(existing))
-			repositoryService.removeLocation(entry);
-
-		addEntry(getScriptStorageLocation(), true);
-		Collection<IEntry> entries = (Collection<IEntry>) tableViewer.getInput();
-
-		for (IEntry entry : entries)
-			repositoryService.addLocation(entry);
-
-		// store script storage location
-		IEclipsePreferences prefs = InstanceScope.INSTANCE.getNode(Activator.PLUGIN_ID);
-		prefs.put(IPreferenceConstants.SCRIPT_STORAGE_LOCATION, getScriptStorageLocation());
+		repositoryService.update(false);
 
 		return super.performOk();
-	}
-
-	private String getScriptStorageLocation() {
-		if (btnUseCustomLocation.getSelection())
-			// custom location
-			return ftext.getText();
-
-		else
-			// default location
-			return IPreferenceConstants.DEFAULT_SCRIPT_STORAGE_LOCATION;
 	}
 }
