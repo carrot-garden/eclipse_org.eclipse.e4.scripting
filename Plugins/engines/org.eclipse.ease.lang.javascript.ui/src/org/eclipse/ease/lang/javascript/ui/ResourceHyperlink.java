0051b952794e0a1b22c0097f6da43d6cb0c42056
Christian Pontesegger <christian.pontesegger@web.de>
2014-06-11 12:42:01 +0200

    * added hyperlink detector for files
      detects workspace files using absolute and relative paths
    
    Change-Id: I85f8106b9bf2df96a012cc07421ecf57aa81f209

---
+++ Plugins/engines/org.eclipse.ease.lang.javascript.ui/src/org/eclipse/ease/lang/javascript/ui/ResourceHyperlink.java
@@ -0,0 +1,53 @@
+package org.eclipse.ease.lang.javascript.ui;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.jface.text.IRegion;
+import org.eclipse.jface.text.hyperlink.IHyperlink;
+import org.eclipse.ui.IEditorDescriptor;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.editors.text.EditorsUI;
+import org.eclipse.ui.part.FileEditorInput;
+
+public class ResourceHyperlink implements IHyperlink {
+
+	private final IRegion fRegion;
+	private final IFile fFile;
+
+	public ResourceHyperlink(final IFile file, final IRegion region) {
+		fFile = file;
+		fRegion = region;
+	}
+
+	@Override
+	public IRegion getHyperlinkRegion() {
+		return fRegion;
+	}
+
+	@Override
+	public String getTypeLabel() {
+		return null;
+	}
+
+	@Override
+	public String getHyperlinkText() {
+		return null;
+	}
+
+	@Override
+	public void open() {
+		IEditorDescriptor descriptor = PlatformUI.getWorkbench().getEditorRegistry().getDefaultEditor(fFile.getName());
+		if (descriptor == null)
+			descriptor = PlatformUI.getWorkbench().getEditorRegistry().findEditor(EditorsUI.DEFAULT_TEXT_EDITOR_ID);
+
+		if (descriptor != null) {
+			final IEditorDescriptor editorDescriptor = descriptor;
+			try {
+				PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().openEditor(new FileEditorInput(fFile), editorDescriptor.getId());
+			} catch (PartInitException e) {
+				// cannot open editor, ignore
+				e.printStackTrace();
+			}
+		}
+	}
+}
